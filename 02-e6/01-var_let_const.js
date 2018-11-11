1d53365e30ff47aa9844cf43d3edcd6f1f74b29e
Daniel Chen <chendaniely@users.noreply.github.com>
2018-11-11 03:16:20 -0500

    Create 01-var_let_const.js

---
+++ 02-e6/01-var_let_const.js
@@ -0,0 +1,24 @@
+var myName = "Dan";
+console.log(myName)
+
+myName = 'Julia';
+console.log(myName)
+
+let myName2 = "Dan";
+console.log(myName)
+
+myName2 = 'Julia';
+console.log(myName)
+
+var myName = "Dan";
+console.log(myName)
+
+myName = 'Julia';
+console.log(myName)
+
+const myName3 = "Dan";
+console.log(myName)
+
+myName3 = 'Julia';
+console.log(myName)
+
