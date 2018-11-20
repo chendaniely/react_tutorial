078a00f8b1970aceae4b6a9e6eb8e718c0057dcd
Daniel Chen <chendaniely@users.noreply.github.com>
2018-11-20 11:17:17 -0500

    Create 08-array_functions.js

---
+++ 02-e6/08-array_functions.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3];
+
+// this is a map function
+const doubleNumArray = numbers.map((pizza) => {
+  return pizza * 2
+});
+
+console.log(numbers); // [1, 2, 3]
+console.log(doubleNumArray); // [2, 4, 6]
